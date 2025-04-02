c382aae86070a7c8dffcd5e86a507396934eff0a
NTB <nguyenthaibao9a1tg2017@gmail.com>
2025-04-03 01:11:48 +0700

    chỉnh sửa cấu trúc

--- BackEnd/Nhom2_WebTourDuLich_user_service/src/main/java/iuh/fit/se/nhom2_webtourdulich_user_service/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package iuh.fit.se.nhom2_webtourdulich_user_service.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
--- BackEnd/Nhom2_WebTourDuLich_user_service/src/main/java/iuh/fit/se/nhom2_webtourdulich_user_service/service/AuthService.java
+++
@@ -1,9 +0,0 @@
-package iuh.fit.se.nhom2_webtourdulich_user_service.service;
-
-import iuh.fit.se.nhom2_webtourdulich_user_service.dto.LoginRequest;
-import iuh.fit.se.nhom2_webtourdulich_user_service.dto.RegisterRequest;
-
-public interface AuthService {
-    String login(LoginRequest loginRequest);
-    Object register(RegisterRequest request);
-}
