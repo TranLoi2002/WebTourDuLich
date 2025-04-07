6b13746194079acfbcf4fe05f86b199232f9e7a1
Nguyen Truong An <nguyentruongan0610@gmail.com>
2025-04-07 23:25:48 +0700

    UPDATE: create component adminSidebar

--- FrontEnd/src/pages/admin/dashboard.js
+++ FrontEnd/src/pages/admin/dashboard.js
@@ -4,6 +4,7 @@ import BookingTable from '../../components/admin/bookingTable';
 import TourTable from '../../components/admin/tourTable';
 import UserTable from '../../components/admin/userTable';
 import SettingModal from '../../components/admin/settingModal';
+import AdminSidebar from '../../components/admin/adminSidebar';
 
 function Dashboard() {
   const [activeTab, setActiveTab] = useState('booking');
@@ -20,86 +21,22 @@ function Dashboard() {
     }
   };
 
-
-
   return (
     <div className="h-fit flex-column">
       <div className="z-10">
+        {/* Navbar */}
         <AdminNavbar />
       </div>
 
       <div className="fixed flex w-full h-full">
         {/* Sidebar */}
-        <div>
-          <div
-            className="top-0 left-0 z-40 w-64 h-screen pt-8 border-r sm:translate-x-0 bg-gray-800 border-gray-700"
-            aria-label="Sidebar"
-          >
-            <div className="h-full px-3 pb-4 overflow-y-auto bg-gray-800">
-              <ul className="space-y-2 font-medium">
-                <li>
-                  <div className="text-white font-bold text-xl mb-4">
-                    <span className="flex-1 ms-3 whitespace-nowrap text-left">Dashboard</span>
-                  </div>
-                </li>
+        <AdminSidebar
+          activeTab={activeTab}
+          setActiveTab={setActiveTab}
+          openSetting={() => setIsSettingOpen(true)}
+        />
 
-                <li>
-                  <button
-                    type="button"
-                    className={`flex items-center w-full p-2 rounded-lg group ${
-                      activeTab === 'booking'
-                        ? 'bg-gray-200 dark:bg-gray-700'
-                        : 'text-gray-300 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700'
-                    }`}
-                    onClick={() => setActiveTab('booking')}
-                  >
-                    <span className="flex-1 ms-3 whitespace-nowrap text-left">Booking</span>
-                  </button>
-                </li>
-
-                <li>
-                  <button
-                    type="button"
-                    className={`flex items-center w-full p-2 rounded-lg group ${
-                      activeTab === 'tour'
-                        ? 'bg-gray-200 dark:bg-gray-700'
-                        : 'text-gray-300 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700'
-                    }`}
-                    onClick={() => setActiveTab('tour')}
-                  >
-                    <span className="flex-1 ms-3 whitespace-nowrap text-left">Tour</span>
-                  </button>
-                </li>
-
-                <li>
-                  <button
-                    type="button"
-                    className={`flex items-center w-full p-2 rounded-lg group ${
-                      activeTab === 'user'
-                        ? 'bg-gray-200 dark:bg-gray-700'
-                        : 'text-gray-300 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700'
-                    }`}
-                    onClick={() => setActiveTab('user')}
-                  >
-                    <span className="ms-3 text-left">User</span>
-                  </button>
-                </li>
-
-                <li>
-                  <button
-                    type="button"
-                    className={`flex items-center w-full p-2 rounded-lg group text-gray-300 dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700`}
-                    onClick={() => setIsSettingOpen(true)}
-                  >
-                    <span className="ms-3 text-left">Setting</span>
-                  </button>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-
-        {/* Content */}
+        {/* Nội dung */}
         <div className="container p-4 overflow-auto">
           {renderTable()}
         </div>
@@ -107,7 +44,6 @@ function Dashboard() {
 
       {/* Setting Modal */}
       {isSettingOpen && <SettingModal onClose={() => setIsSettingOpen(false)} />}
-
     </div>
   );
 }
