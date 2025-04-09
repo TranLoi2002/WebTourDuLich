711b180c650fad1583f051055d0151b1f3209a00
TranLoi2002 <121424271+TranLoi2002@users.noreply.github.com>
2025-04-09 23:53:19 +0700

    chinh sua

--- BackEnd/payment_service/src/main/java/iuh/fit/se/payment_service/controller/PaymentMethodController.java
+++ BackEnd/payment_service/src/main/java/iuh/fit/se/payment_service/controller/PaymentMethodController.java
@@ -2,11 +2,8 @@ package iuh.fit.se.payment_service.controller;
 
 import iuh.fit.se.payment_service.dto.PaymentMethodDTO;
 import iuh.fit.se.payment_service.dto.PaymentMethodResponseDTO;
-import iuh.fit.se.payment_service.entity.Payment;
-import iuh.fit.se.payment_service.entity.PaymentMethod;
 import iuh.fit.se.payment_service.service.PaymentMethodService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -35,8 +32,5 @@ public class PaymentMethodController {
     @GetMapping
     public List<PaymentMethodResponseDTO> getAll() {
         return paymentMethodService.getAll();
-    }
-    public PaymentMethodController(PaymentMethodService paymentMethodService) {
-        this.paymentMethodService = paymentMethodService;
     }
 }
